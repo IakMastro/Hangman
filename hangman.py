5f88bd9ba1df5fda5d45659dba07dcf349c0eb21
Iakovos Mastrogiannopoulos <cse242017102@uniwa.gr>
2019-09-01 19:25:53 +0300

    Finished the project
    Fixed a couple of mistakes and added a few forgotten stuff and a UI for the interaction. It seems everything is running smoothly now.

--- hangman.py
+++ hangman.py
@@ -23,8 +23,9 @@ def Game():
     for letter in word:
         lettersFound.append("_")
 
-    while not Found and numOfTries < 3:
+    while not Found and numOfTries < 10:
 
+        print(f"Number of tries left: {10 - numOfTries}")
         print(f"The word has {len(word)} and you found so far {foundSoFar}")
 
         print("Those are: ", end='')
@@ -35,9 +36,11 @@ def Game():
 
         answer = input("\nGuess a letter: ")
 
-        if answer in word:
+        if answer in word and answer not in lettersFound:
 
             print("You found a letter!")
+
+            Found = True
 
             for i in range(len(word)):
 
@@ -46,13 +49,42 @@ def Game():
                     lettersFound[i] = word[i]
                     foundSoFar += 1
 
+                if Found:
+
+                    if lettersFound[i] != word[i]:
+
+                        Found = False
+
         else:
 
             print("Wrong answer. This letter doesn't exist on the word")
             numOfTries += 1
 
-        if(numOfTries == 3):
+        if numOfTries == 10:
 
             print(f"You lost....\nThe word was \"{word}\".")
 
-Game()
+        if Found:
+
+            print(f"Congrats! You found the word! It's \"{word}\"")
+
+print("Welcome to the hangman python game!")
+gamesPlayed = 0
+
+while(1):
+
+    gamesPlayed += 1
+    print(f"This is your {gamesPlayed} game")
+
+    print("Please wait so the program will pick up the word")
+    Game()
+
+    answer = input("Do you want to continue? y/n?: ")
+
+    if answer == 'n':
+
+        break;
+
+print(f"It was fun playing with {gamesPlayed} games with you!")
+
+print("End of program")
